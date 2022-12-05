0d61b0ee94a2c45a3a8fbf5917433e43c71c9288
Victor <victor-hrj@hotmail.com>
2022-12-05 16:00:52 +0000

    Adicionando Contexto

---
+++ TesteQuestao8/Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteQuestao8.Models;
+
+namespace TesteQuestao8.Data
+{
+    public class Context : DbContext
+
+    {
+        public Context(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> CLiente { get; set; }
+
+
+    }
+}
---
+++ TesteQuestao8/Models/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteQuestao8.Models
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string num_cliente { get; set; }
+        public string nome { get; set; }
+    }
+}
